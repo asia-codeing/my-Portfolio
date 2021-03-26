a86454d488d84ab1f1540cc5b4af4928cdb91100
Asia Alnahi <asia.alius@gmail.com>
2021-03-26 13:57:54 -0700

    adding links and pictures to the cards and added some functionallity to the navmenu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,37 @@
-// function myFunction() {
-//     var x = document.getElementById("myTopnav");
-//     if (x.className === "topnav") {
-//       x.className += " responsive";
-//     } else {
-//       x.className = "topnav";
-//     }
-//   }
+//----Nav menu---
+(() =>{
+   const hamburgerBtn = document.querySelector(".hamburger-btn");
+   const navMenu = document.querySelector(".nav-menu");
+   const closeNavMenu = document.querySelector(".close-nav-menu");
+   hamburgerBtn.addEventListener("click", showMenu);
+   closeNavMenu.addEventListener("click", closeMenu);
+
+   function showMenu(){
+    navMenu.classList.add("open");
+   }
+   function closeMenu(){
+    navMenu.classList.remove("open");
+  }
+
+  document.addEventListener("click",(event) =>{
+      if(event.target.classList.contains("link")){
+         if(event.target.hash !== ""){
+             event.preventDefault();
+             const hashId =event.target.hash;
+             console.log(hashId);
+         } 
+      }else{
+       
+      }
+  })
+})();
+
+//----Hidding all sections except the active
+(() =>{
+    const sections  = document.querySelectorAll(".section");
+    sections.forEach((section) =>{
+    if(!section.classList.contains("active")){
+        section.classList.add("hide");
+    }
+  })
+})();
