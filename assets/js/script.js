8c87e02c95e326b39648cccf53255830feb00fb0
Asia Alnahi <asia.alius@gmail.com>
2021-03-26 15:55:55 -0700

    navigates the sections throuth the navMenu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+document.querySelector(".section").classList.remove("hide");
+document.querySelector(".section").classList.add("active");
+
 //----Nav menu---
 (() =>{
    const hamburgerBtn = document.querySelector(".hamburger-btn");
@@ -5,23 +8,38 @@
    const closeNavMenu = document.querySelector(".close-nav-menu");
    hamburgerBtn.addEventListener("click", showMenu);
    closeNavMenu.addEventListener("click", closeMenu);
-
+   
+   //----Function to open the nav menu-----
    function showMenu(){
     navMenu.classList.add("open");
    }
+
+   //----Function to close the nav menu-----
    function closeMenu(){
     navMenu.classList.remove("open");
   }
-
+  
+  //-----Targeting the sections inthe nav menu---
   document.addEventListener("click",(event) =>{
       if(event.target.classList.contains("link")){
          if(event.target.hash !== ""){
              event.preventDefault();
+             //-----Getting the section Id---
              const hashId =event.target.hash;
-             console.log(hashId);
+             
+             document.querySelector(".section.active").classList.add("hide");
+             document.querySelector(".section.active").classList.remove("active");
+
+             document.querySelector(hashId).classList.add("active");
+             document.querySelector(hashId).classList.remove("hide");
+
+             navMenu.querySelector(".active").classList.add("outer-shadow", "hover-in-shadow");
+             navMenu.querySelector(".active").classList.remove("active", "inner-shadow");
+             event.target.classList.add("active", "inner-shadow");
+             event.target.classList.remove("outer-shadow", "hover-in-shadow");
+             closeMenu();
+             window.location.hash = hashId;
          } 
-      }else{
-       
       }
   })
 })();
